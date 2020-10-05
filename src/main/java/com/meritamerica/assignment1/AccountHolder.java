dcc98e13fcc3b51f624e582284562f532dda5184
Q <quinton79@live.com>
2020-10-04 23:25:39 -0500

    finished assignment1

--- src/main/java/com/meritamerica/assignment1/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment1/AccountHolder.java
@@ -1,5 +1,71 @@
 package com.meritamerica.assignment1;
 
 public class AccountHolder {
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String ssn;
+	private CheckingAccount checkingAccount;
+	private SavingsAccount SavingsAccount;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public String toString() {
+		return "Name: " + firstName + " " + middleName + " " + lastName +
+				"\n" + "SSN: " + ssn +
+				"\n" + "Checking Account Balance: " + checkingAccount.getBalance() + 
+				"\n" + "Checking Account Interest Rate: " + checkingAccount.getInterestRate() + 
+				"\n" + "Checking Account Balance In 3 Years: " + checkingAccount.futureValue(3) +
+				"\n" + "Savings Account Balance: " + SavingsAccount.getBalance() + 
+				"\n" + "Savings Account Interest Rate: " + SavingsAccount.getInterestRate() + 
+				"\n" + "Savings Account Balance In 3 Years: " + SavingsAccount.futureValue(3);
+		
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+	public CheckingAccount getCheckingAccount() {
+		return checkingAccount;
+	}
+	public void setCheckingAccount(CheckingAccount checkingAccount) {
+		this.checkingAccount = checkingAccount;
+	}
+	public SavingsAccount getSavingsAccount() {
+		return SavingsAccount;
+	}
+	public void setSavingsAccount(SavingsAccount savingsAccount) {
+		this.SavingsAccount = savingsAccount;
+	}
+	public AccountHolder(String firstName, String middleName, String lastName, String ssn,
+			double checkingAccount, double savingsAccount) {
+		super();
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.ssn = ssn;
+		this.checkingAccount = new CheckingAccount(checkingAccount);
+		this.SavingsAccount = new SavingsAccount(savingsAccount);
+	}
+	
+	
 	
 }
--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -1,5 +1,48 @@
 package com.meritamerica.assignment1;
 
 public class SavingsAccount {
+	private double balance = 0;
+	private static final double interestRate = 0.01;
 	
+	public double getBalance() {
+		return balance;
+	}
+	public SavingsAccount(double checking) {
+		balance = checking;
+	}
+	public static double getInterestRate() {
+		return interestRate;
+	}
+	public boolean withdraw(double money) {
+		if(balance > money) {
+			balance -= money;
+			System.out.println("You have with drawn " + money + " your current balance is: " + balance);
+			return true;
+		}
+		else {
+			System.out.println("You do not have enough money your balance is: " + balance);
+			return false;
+		}
+	}
+	public boolean deposit(double money) {
+		if(money > 0) {
+			balance = money + balance;
+			System.out.println(money + " Has been added to your balance");
+			return true;
+		}
+		else {
+			System.out.println("This is a negative amount");
+			return false;
+		}
+	}
+	public double futureValue(int years) {
+		double future = 0;
+		future = Math.pow(1 + interestRate, years) * balance;
+		return future;
+	}
+	public String toString() {
+		return "Savings Account Balance: " + balance + 
+				"\n" + "Savings Account Interest Rate: " + interestRate + 
+				"\n" + "Savings Account Balance In 3 Years: " + futureValue(3);
+	}
 }
